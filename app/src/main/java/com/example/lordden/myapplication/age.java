a53a26caf029eb5ae245c534f51df7fcceb4a916
das11 <kabird1196@gmail.com>
2016-07-17 12:16:49 +0530

    flag update

--- app/src/main/java/com/example/lordden/myapplication/age.java
+++ app/src/main/java/com/example/lordden/myapplication/age.java
@@ -8,6 +8,8 @@ import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.KeyEvent;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,10 +18,11 @@ public class age extends AppCompatActivity {
 
     WifiManager wms;
     Boolean thread_kill = false;
-    int delay = 3 * 1000;
+    int delay = 3 * 1000, flag0, flag1;
     List<String> lssid = new ArrayList<String>();
     List<Integer> llev = new ArrayList<Integer>();
     List<Integer> flag = new ArrayList<Integer>();
+    Thread refresh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +34,37 @@ public class age extends AppCompatActivity {
         wms = (WifiManager)getSystemService(Context.WIFI_SERVICE);
         registerReceiver(wifiScanReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
 
+        refresh = new Thread(){
 
+            public void run(){
+                while(!thread_kill){
+                    try{
+                        Thread.sleep(delay);
+                        Log.d("SCAN", "start");
+                        wms.startScan();
+                    }catch (InterruptedException e){
+
+                    }
+                }
+            }
+        };
+
+
+    }
+
+    @Override
+    public boolean onKeyUp(int keycode, KeyEvent event){
+        if (keycode == event.KEYCODE_BACK){
+            onBackPressed();
+            return true;
+        }
+        return super.onKeyUp(keycode, event);
+    }
+
+    @Override
+    public void onBackPressed(){
+        thread_kill = true;
+        finish();
     }
 
 
@@ -58,21 +91,30 @@ public class age extends AppCompatActivity {
         boolean flag_match = false;
         List<String> true_ssid = new ArrayList<String>();
         List<Integer> true_lev = new ArrayList<Integer>();
-        int count = 0, i = 0;
+        int count = 0, c = 0;
 
-        for(int i = 0; i < lssid.size(); ++i){
+        for(int i = 0 ; i < lssid.size(); ++i){
             if (ssid.get(count).toString().equals(lssid.get(i).toString())){
                 true_ssid.add(ssid.get(count).toString());
                 true_lev.add(dbm.get(count));
                 ++count;
+            }else{
+                continue;
             }
         }
 
+        flag0 = 1;
+        flag1 = 1;
+
         for (int j = 0; j < true_ssid.size(); ++j){
-                if (true_lev.get(j) < llev.get(i)){
-                    flag.add(i, 0);
-                }else if (true_lev.get(j) > llev.get(i)){
-                    flag.add(i, 1);
+                if (true_lev.get(j) < llev.get(c)){
+                    flag.add(c, 0);
+                    ++c;
+                    ++flag0;
+                }else if (true_lev.get(j) > llev.get(c)){
+                    flag.add(c, 1);
+                    ++c;
+                    ++flag1;
                 }
         }
 
